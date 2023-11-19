a1006681c869a008a0cf0d016589b3c938b2209d
diansopandi <dian.sopandi@nalysa.com>
2023-11-19 16:48:12 +0700

    about form revise

--- components/About.tsx
+++ components/About.tsx
@@ -13,22 +13,18 @@ const About = () => {
         </p>
         <div className="text-gray-600 col-span-2">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore,
-            quidem quos. Nobis temporibus laudantium, aliquam aut assumenda
-            beatae unde quam ex praesentium corrupti, mollitia quos inventore
-            soluta. Laudantium facere amet consequuntur inventore, distinctio
-            molestiae dolor quibusdam, minus, rerum perspiciatis eveniet?
-            Aliquam quidem laborum ut provident voluptatum rem aperiam, tenetur
-            asperiores!
+            Todays i&apos;m spending a lot of times and focusing on web
+            development with using Javascript technologies both for website or
+            mobile applications. I have got already experiences with several
+            technologies later for desktop or web development, such as delphi or
+            php for several years. also i have got already experiences with
+            several databases relational database management system such as
+            mysql and non relational database also well known as noSQL such as
+            mongodb
           </p>
         </div>
         <div className="text-gray-600 col-span-1">
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis
-            aliquid dolore enim nesciunt? Et optio veritatis ipsum rem a
-            excepturi aliquid, ullam voluptates minus dolorem laboriosam quidem,
-            itaque tempore cum.
-          </p>
+          <p></p>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center hover:scale-105 ease-in duration-300 hidden">
           <Image
